3f00dbcd4756c5882b36037623009235ac7608a8
welder86 <112901748+welder86@users.noreply.github.com>
2023-02-25 14:20:44 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,40 @@
+"use strict"
+// console.log('ssdfgf');
+// console.log('Привіт Я тут');
+
+// let old = 36;
+
+// console.log('old');
+
+
+// let size = 400;
+// console.log(size);
+// let size2;
+// size2 = size;
+// console.log(size2);
+
+// const ageOld = 132;
+// console.log(ageOld);
+
+
+// const COLOR_RED = '#498635';
+// console.log(COLOR_RED);
+
+
+
+// console.log('Вчимо JS');
+// ['Вчимо', 'JS'].forEach(alert);
+
+// console.log('Вчимо' + 'JS');
+
+ let num = 4;
+ console.log(num);
+
+function name() {
+   console.log('Вчимо');
+   console.log('JS');
+}
+name();
+
+console.log('123');
+console.log('Вчу JS')
